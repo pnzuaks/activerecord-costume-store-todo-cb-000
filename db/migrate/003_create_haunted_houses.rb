e24e8227cfcf8c467feadfd44c588697b76bf9b6
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 19:53:07 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,13 +7,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-
-
-      t.integer :number_of_costumes
-      t.integer :number_of_employees
-      t.boolean :in_business
-      t.float :opening_time
-      t.float :closing_time
+      t.float :opening_date
+      t.float :closing_date
+      t.string :long_description
   end
   end
 end
