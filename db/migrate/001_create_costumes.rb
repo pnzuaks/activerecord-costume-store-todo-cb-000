36ddef151a006d12a13a7e1137d6a4fd23c0055a
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 19:44:31 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,9 +13,8 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.string :director
-      t.string :lead
-      t.boolean :in_theaters
+      t.string :size
+      t.string :image_url
   end
   end
 end
