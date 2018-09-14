e4a70474e72b03b22418f726cb6ef332e8910d75
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 19:50:26 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.boolean :in_business
+      t.float :opening_time
+      t.float :closing_time
+  end
+  end
+end
