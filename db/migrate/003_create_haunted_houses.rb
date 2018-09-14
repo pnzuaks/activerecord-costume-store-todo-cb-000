10454827cbb074ac71da191c2af7ee9d58b603f4
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 20:23:28 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,6 +10,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :long_description
+
+      t.timestamps
+
   end
   end
 end
