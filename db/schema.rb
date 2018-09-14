283aeee871300f74e70f305ebaf481fdce2136a1
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 20:28:56 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
-
-  create_table "costume_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "number_of_costumes"
-    t.integer "number_of_employees"
-    t.boolean "in_business"
-    t.float "opening_time"
-    t.float "closing_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
