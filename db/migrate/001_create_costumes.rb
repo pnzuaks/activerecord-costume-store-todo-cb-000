8dfc5b24232d64a307ff0643c8a699c99450ec6e
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 19:44:06 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,9 +10,9 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
-    create_table :movies do |t|
-      t.string :title
-      t.integer :release_date
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
       t.string :director
       t.string :lead
       t.boolean :in_theaters
