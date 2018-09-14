0e5603dcf97a320a1ac5be85797573d6abd2d693
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 19:51:16 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,9 +1,12 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
-    create_table :costume_stores do |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :location
+      t.string :theme
+      t.inject(Array.new) { |arr, a| arr.push(*a) }
+
       t.integer :number_of_costumes
       t.integer :number_of_employees
       t.boolean :in_business
