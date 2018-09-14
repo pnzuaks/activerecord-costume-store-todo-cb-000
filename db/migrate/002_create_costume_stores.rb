1673c0ed0a9fc6bdc5f3af3c7ca3ff72f1fed5b3
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 20:32:50 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.float :opening_time
-      t.float :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
       t.timestamps
 
   end
