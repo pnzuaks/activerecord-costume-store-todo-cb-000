8b1a478258d20f3e4f75a0f7bb19ac5ecfd7b6c2
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 19:47:42 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+  end
+  end
+end
